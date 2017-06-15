453cb01c0e524a3316d2c002a3a16eb5e9c654d4
antweer <trajwani@gmail.com>
2017-06-15 09:49:15 -0500

    Resetting key

--- api.js
+++ api.js
@@ -12,12 +12,11 @@ app.get('/', function(request, response){
   response.render('axios.hbs', {});
 });
 
+var DARK = process.env.DARKSKY_KEY
 app.get('/api/:lat/:long', cache('5 minutes'), function(request, response, next){
   var lat = request.params.lat;
   var long = request.params.long;
-  console.log(lat, long);
-  console.log('https://api.darksky.net/forecast/46d56008ef16b0c42e29b008cf538aac/'+lat+','+long);
-  axios.get('https://api.darksky.net/forecast/46d56008ef16b0c42e29b008cf538aac/'+lat+','+long).then(function(r){
+  axios.get('https://api.darksky.net/forecast/' + DARK + '/'+lat+','+long).then(function(r){
       response.json(r.data);
     })
     .catch(next);
